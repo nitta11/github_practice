8c00f09363880f32e4928bf584a1d4868e29fd23
hogehogerin <is668vwsbhds@sute.jp>
2017-08-29 14:32:10 -0400

    fix utf8 problem

--- username.py
+++ username.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
+import sys
+import codecs
 import datetime
+
+sys.stdout = codecs.getwriter('utf_8')(sys.stdout)
 
 username = 'hogehogerin'
 print(u'こんにちは%s。現在時刻は%sです。' % (username, datetime.datetime.now()))
